8fe1d68db8810a274bb823f38c705cc6f7bf2ab0
ispp2105 <1234@1234-123>
2026-03-04 14:24:37 +0300

    lr1112

---
+++ lections/Terminal/Terminal/Program.cs
@@ -0,0 +1,16 @@
+﻿using Terminal.Gui;
+
+Application.Init();
+
+using Window window = new() { Title = "Hello Nigger (Esc to quit)" };
+Label label = new()
+{
+    Text = "Hello, Terminal.Gui v2!",
+    X = Pos.Center(),
+    Y = Pos.Center()
+};
+window.Add(label);
+
+var 
+
+Application.Run(window);
